d60a6d61f5315bd4cae36c0420b2a8a6d35f40c6
KJTempas <fw6927mm@go.minneapolis.edu>
2020-09-17 14:40:44 -0500

    add models for dbase tables Artist and Artwork

---
+++ models.py
@@ -0,0 +1,33 @@
+from peewee import *
+
+db = SqliteDatabase('art.sqlite') #creating an instance of a database
+
+class Artist(Model):
+    name = CharField(unique=True) #fields in artist table
+    email = CharField(unique=True)
+    #id = IntegerField()  ? but is autoassigned
+
+    #link this model to art dbase
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return f'The artist {self.name} with contact info {self.email} is in the artworks catalog.'
+
+
+class Artwork(Model):
+    artist = ForeignKeyField(Artist)# , backref='artworks') #not sure about backrefs 
+    #artworkid - autoincrement?
+    name_of_artwork = CharField()   #unique
+    price = DecimalField(9,2)
+    is_available = BooleanField(default=True)
+
+    class Meta:
+        database = db
+    
+    def __str__(self):
+        return f'{self.name_of_artwork} is {self.is_available} available for {self.price}'
+
+#connect to DB and create tables that map to models Artist and Artwork
+db.connect()
+db.create_tables([Artist, Artwork])
