4b705ab5d5c71af5c55a54a6092c6e2f10352ead
KJTempas <fw6927mm@go.minneapolis.edu>
2020-09-17 20:13:43 -0500

    setup outline of methods

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def create_menu():
     menu = Menu()
     menu.add_option('1', 'Add New Artist', add_artist)
     menu.add_option('2', 'Add Artwork', add_artwork)
-    menu.add_option('3', 'Display available work by one artist', display_available_art)
+    menu.add_option('3', 'Display available work by an artist', display_available_art_by_an_artist)
     menu.add_option('4', 'Show all work by one artist', show_all_by_one_artist)
     menu.add_option('5', 'Change availability status of artwork', change_availability)
     menu.add_option('6', 'Delete an artwork', delete_artwork)
@@ -32,3 +32,30 @@ def add_artist():
         new_artist.save()
     except Exception as e:
         art_ui.message('Error: Artist Already on File')
+
+def add_artwork():
+    pass
+
+
+def display_available_work_by_an_artist():
+    pass
+
+
+def show_all_by_one_artist():
+    pass
+
+def change_availability():
+    pass
+    #artwork_id = art_ui.get_artwork_id()
+    #artwork = artwork.get_artwork_by_id(artwork_id)
+
+
+def delete_artwork():
+    pass
+
+def quit_program():
+    art_ui.message('Thanks and bye!')
+
+
+if __name__== '__main__':
+    main()
